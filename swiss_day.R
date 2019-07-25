904450f46327a33e674cf641797b514421769a41
Julien Brun <brun.julien@gmail.com>
2019-07-25 15:30:46 -0700

    better checks and better comments

--- swiss_day.R
+++ swiss_day.R
@@ -1,7 +1,7 @@
 
 #' For a specific year, return the day of the week at which the Swiss National Day will occur
 #'
-#' @param yoi A number or Character -- Year you want to search.
+#' @param yoi A number or character -- Year you want to search.
 #'
 #' @return Day of the week
 #' @export
@@ -10,14 +10,14 @@
 #' swiss_day(2019)
 #' 
 swiss_day <- function(yoi){
-  
-  # Transform the year as characted in cas it was apssed as an integer
-  yoic <- as.character(yoi)
-  
-  # Check if the year has 4 digits
+  # Check if the parameter can be transfomed into a 4 digit number
+  stopifnot(is.na(as.integer(yoic)))
   stopifnot(nchar(yoic)==4)
   
-  # Create the Swiss national day date (first of August)
+  # Transform the year as character in case it was passed as an integer
+  yoic <- as.character(yoi)
+  
+  # Create the Swiss national day date (first of August -- although everybody should know that)
   swiss_date <- lubridate::ymd(paste(yoic, "08", "01", sep = "-"))
   
   # Get the day of the week
