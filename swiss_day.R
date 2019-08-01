b8546ee293f7a055c7142be3809553591d4f1205
jeremyknox-ucsb <knox@aurora.nceas.ucsb.edu>
2019-08-01 14:04:59 -0700

    fixed error handling

--- swiss_day.R
+++ swiss_day.R
@@ -10,16 +10,19 @@
 #' swiss_day(2019)
 #' 
 swiss_day <- function(yoi){
-  # Check if the parameter can be transfomed into a 4 digit number
-  stopifnot(is.na(as.integer(yoic)))
-  stopifnot(nchar(yoic)==4)
-  
+
   # Transform the year as character in case it was passed as an integer
   yoic <- as.character(yoi)
+  
+  # Check if the parameter can be transfomed into a 4 digit number
+  stopifnot(!is.na(as.integer(yoic)))
+  stopifnot(nchar(yoic)==4)
   
   # Create the Swiss national day date (first of August -- although everybody should know that)
   swiss_date <- lubridate::ymd(paste(yoic, "08", "01", sep = "-"))
   
   # Get the day of the week
-  lubridate::wday(swiss_date, label =TRUE, abbr = FALSE)
+  d <- lubridate::wday(swiss_date, label =TRUE, abbr = FALSE)
+  
+  
 }
